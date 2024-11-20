cebfb947e70c37965318305918139361b9295e89
ub2204 pikachu <ub2204 pikachu>
2024-11-20 16:20:07 +0800

    [bugfix] fix to use sigmoid for 2d plain region problem; adjust data domain range to positive region

--- trial_task_01/main.py
+++ trial_task_01/main.py
@@ -31,8 +31,8 @@ _ = os.system('clear')
 
 model = keras.Sequential(
     [
-        layers.Dense(3, input_dim=2, activation="relu", name="layer1"),
-        layers.Dense(2, input_dim=3, name="layer2"),
+        layers.Dense(3, input_dim=2, activation="sigmoid", name="layer1"),
+        layers.Dense(2, input_dim=3, activation="sigmoid", name="layer2"),
     ]
 )
 # Call model on a test input
@@ -61,15 +61,15 @@ print(model.summary())
 # TRAINING DATA GENERATION
 # ----------------------------------------
 SAMPLE_NUM = 12800
-x1         = np.random.rand(SAMPLE_NUM) * 2 - 1 # generate (0, ~N) distribution samples within [-1, +1]
-x2         = np.random.rand(SAMPLE_NUM) * 2 - 1 # generate (0, ~N) distribution samples within [-1, +1]
+x1         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
+x2         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
 x_train    = np.array(
     [ [_1, _2] for _1, _2 in zip(x1, x2) ]
 )
 print(x_train)
 y_train    = np.array(
     [
-        [+1, -1] if x_train_curr[0] * x_train_curr[1] >=0 else [-1, +1] for x_train_curr in x_train
+        [+1,  0] if x_train_curr[0] >= 0.5 and x_train_curr[1] >= 0.5 else [ 0, +1] for x_train_curr in x_train
     ]
 )
 print(y_train)
