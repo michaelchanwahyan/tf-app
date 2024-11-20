94e82723047e7cf8d07c1d9a7d34fb6213fb1849
pikachu-mini <>
2024-11-20 22:34:25 +0800

    [revamp] model generation moved to func; add evaluate and predict

--- trial_task_01/main.py
+++ trial_task_01/main.py
@@ -5,95 +5,144 @@ import tensorflow as tf
 import keras
 from keras import layers
 
-_ = os.system('clear')
 
-# ----------------------------------------
-# DEFINE MODEL STRUCTURE
-# ----------------------------------------
+def get_uncompiled_model():
+    # Define Sequential model with 2 layers
+    #
+    #  INPUT LAYER          LAYER 1           LAYER 2
+    #                  ------ [] ------------
+    #                 /                      \
+    #           ------                        []  ->
+    #          /                             /
+    #    ->  [] ------------     /-----------
+    #                        \  /
+    #                         []
+    #                        /  \
+    #    ->  [] ------------     \-----------
+    #          \                             \
+    #           ------                        []  ->
+    #                 \                      /
+    #                  ------ [] ------------
+    #
 
-# Define Sequential model with 2 layers
-#
-#  INPUT LAYER          LAYER 1           LAYER 2
-#                  ------ [] ------------
-#                 /                      \
-#           ------                        []  ->
-#          /                             /
-#    ->  [] ------------     /-----------
-#                        \  /
-#                         []
-#                        /  \
-#    ->  [] ------------     \-----------
-#          \                             \
-#           ------                        []  ->
-#                 \                      /
-#                  ------ [] ------------
-#
+    model = keras.Sequential(
+        [
+            layers.Dense(3, input_shape=(2,), activation="sigmoid", name="layer1"),
+            layers.Dense(2, input_shape=(3,), activation="sigmoid", name="layer2"),
+        ]
+    )
+    # Call model on a test input
+    #x = tf.ones((1, 2))
+    #y = model(x)
 
-model = keras.Sequential(
-    [
-        layers.Dense(3, input_shape=(2,), activation="sigmoid", name="layer1"),
-        layers.Dense(2, input_shape=(3,), activation="sigmoid", name="layer2"),
-    ]
-)
-# Call model on a test input
-#x = tf.ones((1, 2))
-#y = model(x)
-
-#print(model.weights)
-print(model.summary())
+    #print(model.weights)
+    print(model.summary())
+    return model
 
 
-
-#        REGION          REGION
-#        SYMBOL          SYMBOL
-#      [ -1 , +1 ]  ^  [ +1 , -1 ]
-#                   |
-#                   |
-#                   |
-#           --------+------->
-#                   |
-#        REGION     |    REGION
-#        SYMBOL     |    SYMBOL
-#      [ +1 , -1 ]     [ -1 , +1 ]
+def get_compiled_model(model):
+    model.compile(
+        loss         = keras.losses.CategoricalCrossentropy(),
+        optimizer    = keras.optimizers.Adam(learning_rate=1e-3),
+        metrics      = ["accuracy"]
+    )
+    return model
 
 
-# ----------------------------------------
-# TRAINING DATA GENERATION
-# ----------------------------------------
-SAMPLE_NUM = 12800
-x1         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
-x2         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
-x_train    = np.array(
-    [ [_1, _2] for _1, _2 in zip(x1, x2) ]
-)
-print(x_train)
-y_train    = np.array(
-    [
-        [+1,  0] if x_train_curr[0] >= 0.5 and x_train_curr[1] >= 0.5 else [ 0, +1] for x_train_curr in x_train
-    ]
-)
-print(y_train)
+def gen_training_data_xy(
+        sample_num,
+        verbose = False
+    ):
+    #        REGION          REGION
+    #        SYMBOL          SYMBOL
+    #      [ -1 , +1 ]  ^  [ +1 , -1 ]
+    #                   |
+    #                   |
+    #                   |
+    #           --------+------->
+    #                   |
+    #        REGION     |    REGION
+    #        SYMBOL     |    SYMBOL
+    #      [ +1 , -1 ]     [ -1 , +1 ]
+    x1         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
+    x2         = np.random.rand(SAMPLE_NUM) # generate (0.5, ~N) distribution samples within [0, +1]
+    x_train    = np.array(
+        [ [_1, _2] for _1, _2 in zip(x1, x2) ]
+    )
+    if verbose:
+        print(x_train)
+    y_train    = np.array(
+        [
+            [+1,  0] if x_train_curr[0] >= 0.5 and x_train_curr[1] >= 0.5 else [ 0, +1] for x_train_curr in x_train
+        ]
+    )
+    if verbose:
+        print(y_train)
+    return x_train, y_train
 
-# ----------------------------------------
-# DEFINE MODEL TRAINING PARAMS
-# ----------------------------------------
-model.compile(
-    loss         = keras.losses.CategoricalCrossentropy(),
-    optimizer    = keras.optimizers.Adam(learning_rate=1e-3),
-    metrics      = ["accuracy"]
-)
 
-BATCH_SIZE    = 128
-EPOCHS        = 40
+def gen_testing_data_xy(
+        sample_num,
+        verbose = False
+    ):
+    x_test, y_test = gen_training_data_xy(sample_num, verbose)
+    return x_test, y_test
 
-# ----------------------------------------
-# MODEL FITTING
-# ----------------------------------------
-model.fit(
-    x_train,
-    y_train,
-    batch_size       = BATCH_SIZE,
-    epochs           = EPOCHS,
-    validation_split = 0.1
-)
+
+if __name__ == "__main__":
+    _ = os.system('clear')
+
+
+    # ----------------------------------------
+    # DEFINE MODEL STRUCTURE
+    # ----------------------------------------
+    model = get_uncompiled_model()
+
+
+    # ----------------------------------------
+    # COMPILE MODEL
+    # ----------------------------------------
+    model = get_compiled_model(model)
+
+
+
+
+    # ----------------------------------------
+    # TRAINING DATA GENERATION
+    # ----------------------------------------
+    SAMPLE_NUM = 12800
+    x_train, y_train = gen_training_data_xy(SAMPLE_NUM)
+
+
+    # ----------------------------------------
+    # DEFINE MODEL TRAINING PARAMS
+    # ----------------------------------------
+    BATCH_SIZE    = 128
+    EPOCHS        = 40
+
+
+    # ----------------------------------------
+    # MODEL FITTING
+    # ----------------------------------------
+    model.fit(
+        x_train,
+        y_train,
+        batch_size       = BATCH_SIZE,
+        epochs           = EPOCHS,
+        validation_split = 0.1
+    )
+
+
+    # ----------------------------------------
+    # MODEL EVALUATION
+    # ----------------------------------------
+    x_test, y_test = gen_testing_data_xy(1280)
+    results = model.evaluate(x_test, y_test, )
+    print("test loss, test acc:", results)
+
+
+    x_predict = np.array([[0.3, 0.3],
+                          [0.7, 0.7]])
+    prediction = model.predict(x_predict)
+    print(x_predict, prediction)
 
