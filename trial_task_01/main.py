8029b5769f88eb21d037833600da2bc49b731210
ub2204 pikachu <ub2204 pikachu>
2024-11-20 16:00:13 +0800

    [update] add network params definition and data generation

--- trial_task_01/main.py
+++ trial_task_01/main.py
@@ -1,10 +1,15 @@
 #!/usr/loca/bin/python3
 import os
+import numpy as np
 import tensorflow as tf
 import keras
 from keras import layers
 
 _ = os.system('clear')
+
+# ----------------------------------------
+# DEFINE MODEL STRUCTURE
+# ----------------------------------------
 
 # Define Sequential model with 2 layers
 #
@@ -36,4 +41,59 @@ model = keras.Sequential(
 
 #print(model.weights)
 print(model.summary())
+
+
+
+#        REGION          REGION
+#        SYMBOL          SYMBOL
+#      [ -1 , +1 ]  ^  [ +1 , -1 ]
+#                   |
+#                   |
+#                   |
+#           --------+------->
+#                   |
+#        REGION     |    REGION
+#        SYMBOL     |    SYMBOL
+#      [ +1 , -1 ]     [ -1 , +1 ]
+
+
+# ----------------------------------------
+# TRAINING DATA GENERATION
+# ----------------------------------------
+SAMPLE_NUM = 12800
+x1         = np.random.rand(SAMPLE_NUM) * 2 - 1 # generate (0, ~N) distribution samples within [-1, +1]
+x2         = np.random.rand(SAMPLE_NUM) * 2 - 1 # generate (0, ~N) distribution samples within [-1, +1]
+x_train    = np.array(
+    [ [_1, _2] for _1, _2 in zip(x1, x2) ]
+)
+print(x_train)
+y_train    = np.array(
+    [
+        [+1, -1] if x_train_curr[0] * x_train_curr[1] >=0 else [-1, +1] for x_train_curr in x_train
+    ]
+)
+print(y_train)
+
+# ----------------------------------------
+# DEFINE MODEL TRAINING PARAMS
+# ----------------------------------------
+model.compile(
+    loss         = keras.losses.CategoricalCrossentropy(),
+    optimizer    = keras.optimizers.Adam(learning_rate=1e-3),
+    metrics      = ["accuracy"]
+)
+
+BATCH_SIZE    = 128
+EPOCHS        = 10
+
+# ----------------------------------------
+# MODEL FITTING
+# ----------------------------------------
+model.fit(
+    x_train,
+    y_train,
+    batch_size       = BATCH_SIZE,
+    epochs           = EPOCHS,
+    validation_split = 0.1
+)
 
