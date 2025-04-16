3c2720b646f0defd3e09b7a7edbcfa10172a6960
pikachu <michaelchan_wahyan@yahoo.com.hk>
2025-04-16 13:54:10 +0800

    [update] viz two class label results

--- trial_01_NonlinearClassifier/viz_inference.py
+++ trial_01_NonlinearClassifier/viz_inference.py
@@ -17,24 +17,32 @@ y_ = np.linspace(-0.5,0.5,51)
 xx_, yy_ = np.meshgrid(x_, y_)
 
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+fig, (ax1, ax2) = plt.subplots(1,2,subplot_kw={"projection": "3d"})
 
 # Plot the surface.
-surf = ax.plot_surface(
+surf1 = ax1.plot_surface(
     xx_, yy_, RES[:,0].reshape((51,51)),
     cmap=cm.coolwarm,
     linewidth=0,
     antialiased=False
 )
+surf2 = ax2.plot_surface(
+    xx_, yy_, RES[:,1].reshape((51,51)),
+    cmap=cm.coolwarm,
+    linewidth=0,
+    antialiased=False
+)
 
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-# A StrMethodFormatter is used automatically
-ax.zaxis.set_major_formatter('{x:.02f}')
+for ax in [ax1, ax2]:
+    # Customize the z axis.
+    ax.set_zlim(-1.01, 1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    # A StrMethodFormatter is used automatically
+    ax.zaxis.set_major_formatter('{x:.02f}')
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surf1, shrink=0.5, aspect=5)
+fig.colorbar(surf2, shrink=0.5, aspect=5)
 
 plt.show()
 
