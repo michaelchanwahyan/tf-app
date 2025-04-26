26ebfafe544645f586f44bb413c948c07876867a
pikachu-mini <>
2025-04-27 00:03:22 +0800

    [update] add model; add tb launch script; add gitignore data

--- trial_04_SurfaceAudioClassification/main_train.py
+++ trial_04_SurfaceAudioClassification/main_train.py
@@ -24,7 +24,7 @@ def get_raw_audio_data():
     return sample_rate, surfaceA_audio, surfaceB_audio, airnoise_audio
 
 
-def get_stft_from_audio(inAudio, stft_fftNum=128, stft_progression=1600):
+def get_stft_from_audio(inAudio, stft_fftNum=128, stft_progression=800):
     # -------------------------------------------------------------------------
     #
     # STFT Spectrum  _
@@ -89,6 +89,91 @@ def get_stft_from_audio(inAudio, stft_fftNum=128, stft_progression=1600):
     return X
 
 
+def gen_training_data(
+    sampleNum_per_class,
+    surfaceA_STFT,
+    surfaceB_STFT,
+    airnoise_STFT
+):
+    [M, N] = airnoise_STFT.shape
+    print(f'MxN : {M} x {N}')
+
+    x_train = []
+    y_train = []
+
+    # randomly sample sampleNum_per_class entries
+    randSampleIdx = random.sample(range(N-M), sampleNum_per_class)
+
+    for randIdx in randSampleIdx:
+        # airnoise data
+        x_train.append(airnoise_STFT[:, randIdx : randIdx + M])
+        y_train.append([1,0,0])
+        # surfaceA data
+        x_train.append(surfaceA_STFT[:, randIdx : randIdx + M])
+        y_train.append([0,1,0])
+        # surfaceB data
+        x_train.append(surfaceB_STFT[:, randIdx : randIdx + M])
+        y_train.append([0,0,1])
+
+    x_train = np.array(x_train) / 65536.0
+    y_train = np.array(y_train)
+
+    x_train, y_train = shuffle(x_train, y_train)
+    return x_train, y_train
+
+
+def get_uncompiled_model():
+    input_shape = (128, 128, 1)
+    model = keras.Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Conv2D( 32, (3,3), activation='relu', padding='same'),
+            layers.Conv2D( 32, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D((2,2), strides=(2,2)),
+            layers.Conv2D( 64, (3,3), activation='relu', padding='same'),
+            layers.Conv2D( 64, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D((2,2), strides=(2,2)),
+            layers.Flatten(),
+            layers.Dense( 32, activation='relu'),
+            layers.Dropout(0.5),
+            layers.Dense(  3, activation='relu'),
+        ]
+    )
+    print(model.summary())
+    return model
+
+
+def get_compiled_model(model):
+    model.compile(
+        loss         = 'mse',
+        optimizer    = 'adam',
+        metrics      = ["accuracy"]
+    )
+    return model
+
+
+def get_tsb_ckp_cbk():
+    # Load Tensorboard callback
+    tensorboard = TensorBoard(
+      log_dir=os.path.join(os.getcwd(), "logs"),
+      histogram_freq=1,
+      write_images=True
+    )
+
+    # Save a model checkpoint after every epoch
+    checkpoint = ModelCheckpoint(
+        os.path.join(os.getcwd(), "model_checkpoint"),
+        save_freq="epoch"
+    )
+
+    # Add callbacks to list
+    callbacks = [
+      tensorboard,
+      checkpoint
+    ]
+    return callbacks
+
+
 if __name__ == '__main__':
 
     # read audio dataset
@@ -98,3 +183,29 @@ if __name__ == '__main__':
     surfaceA_STFT = get_stft_from_audio(surfaceA_audio)
     surfaceB_STFT = get_stft_from_audio(surfaceB_audio)
     airnoise_STFT = get_stft_from_audio(airnoise_audio)
+
+    # training data creation
+    sampleNum_per_class = 1000
+    x_train, y_train = \
+        gen_training_data(
+            sampleNum_per_class,
+            surfaceA_STFT,
+            surfaceB_STFT,
+            airnoise_STFT,
+        )
+
+    model = get_uncompiled_model()
+    model = get_compiled_model(model)
+
+    BATCH_SIZE    = 1
+    EPOCHS        = 100
+
+    model.fit(
+        x_train,
+        y_train,
+        callbacks        = get_tsb_ckp_cbk(),
+        batch_size       = BATCH_SIZE,
+        epochs           = EPOCHS,
+        validation_split = 0.1
+    )
+
